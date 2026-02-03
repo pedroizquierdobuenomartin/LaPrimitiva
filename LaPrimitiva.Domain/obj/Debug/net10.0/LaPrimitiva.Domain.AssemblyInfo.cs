9cea16d435298b37169cb778694da513433ee7d5
pedroizquierdobueno <pedro.izquierdobueno@efclif.com>
2026-02-03 15:09:27 +0100

    Resize Sidebar to Adaptive Width

--- LaPrimitiva.Domain/obj/Debug/net10.0/LaPrimitiva.Domain.AssemblyInfo.cs
+++ LaPrimitiva.Domain/obj/Debug/net10.0/LaPrimitiva.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaPrimitiva.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0377f0c23debac45ea12f7d5b328fb9afb6ddf2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83dae4c184677e691a9a9cdbac1fca92784736f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaPrimitiva.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaPrimitiva.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LaPrimitiva.Infrastructure/ToDo List.md
+++
@@ -1,1 +0,0 @@
-- [ ] Mejorar Look & Feel

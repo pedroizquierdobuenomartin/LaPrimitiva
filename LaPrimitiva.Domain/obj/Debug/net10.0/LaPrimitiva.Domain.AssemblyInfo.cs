d920161119ecf8238ab2e2858d59f5c92d41b73d
pedroizquierdobueno <pedro.izquierdobueno@efclif.com>
2026-02-03 16:21:22 +0100

    Mobile Optimization for Plans Section

--- LaPrimitiva.Domain/obj/Debug/net10.0/LaPrimitiva.Domain.AssemblyInfo.cs
+++ LaPrimitiva.Domain/obj/Debug/net10.0/LaPrimitiva.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaPrimitiva.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc8b63d5cedfc44ff99430e0dcdf65482a0ad29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82612ecb8e13fe56b59e4a1743cc621ce6b7dc26")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaPrimitiva.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaPrimitiva.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LaPrimitiva.Tests/obj/Debug/net10.0/LaPrimitiva.Tests.AssemblyInfo.cs
+++ LaPrimitiva.Tests/obj/Debug/net10.0/LaPrimitiva.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaPrimitiva.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17efc876633f2335391e225737d695acf425ade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82612ecb8e13fe56b59e4a1743cc621ce6b7dc26")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaPrimitiva.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaPrimitiva.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

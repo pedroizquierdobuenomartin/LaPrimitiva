90419e4be8c2acbaf2d64b4c1afe17b5eacfa818
pedroizquierdobueno <pedro.izquierdobueno@efclif.com>
2026-02-05 09:43:36 +0100

    Branded 404 Page Enhancement

--- LaPrimitiva.Infrastructure/obj/Debug/net10.0/LaPrimitiva.Infrastructure.AssemblyInfo.cs
+++ LaPrimitiva.Infrastructure/obj/Debug/net10.0/LaPrimitiva.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaPrimitiva.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c28fca725c296b4952bc096b3f290c5318e4521e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d37498b20916590162837e8d0bfbd6da3f6c47e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaPrimitiva.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaPrimitiva.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
